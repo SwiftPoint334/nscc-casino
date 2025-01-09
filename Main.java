fa74c5cdababf8c18b3ff368854b019bb8e7cde2
Parker Doggett <parker@doggett.one>
2025-01-09 09:08:22 -0400

    Basic logic added, few bugs

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
 
         System.out.println("Welcome to the Casino!");
         System.out.println("Please enter the amount of chips you would like to purchase. (Must be between 20 - 200 and a multiple of 10.)");
@@ -32,6 +34,44 @@ public class Main {
             switch (menu_input) {
                 case "1":
                     System.out.println("Option 1 selected");
+
+                    int bankerNumber = (int) rand.nextInt(1001);
+
+                    System.out.println("A number between 0 and 1000 has been chosen.");
+                    System.out.println("You have 10 guesses, the prize is 11 chips minus the number of guesses it took.");
+                    System.out.println("If you run out of guesses, you lose 10 chips.");
+
+                    for (int i = 0; i < 10; i++) {
+                        int guess;
+                        System.out.println("Enter your guess (Guess # " + i + " )");
+                        try {
+                            guess = scanner.nextInt();
+                            scanner.nextLine();
+                        } catch (InputMismatchException e) {
+                            System.out.println("Invalid input, enter an integer.");
+                            scanner.nextLine();
+                            break;
+                        }
+                        if (guess > 1000) {
+                            System.out.println("That was not correct, remember that you need to guess a number between 0 and 1000.");
+                        } else if (guess == bankerNumber) {
+                            System.out.println("That is correct!");
+                            int prize = 11 - i;
+                            chip_balance += prize;
+                            System.out.println("You have been awarded " + prize + " chips. Your balance is now " + chip_balance + " chips.");
+                            break;
+                        } else {
+                            System.out.println("Sorry, that was not correct - try again.");
+                            if (i >= 10) {
+                                System.out.println("You have run out of guesses, 10 chips will be deducted from your balance.");
+                                chip_balance -= 10;
+                                break;
+                            }
+                        }
+                    }
+
+
+
                     break;
                 case "2":
                     System.out.println("Option 2 selected");
